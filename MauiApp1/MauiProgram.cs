400ab9cff947e4a8d60c9315f5efff2481dc2f48
Steve Fan <29133953+stevefan1999-personal@users.noreply.github.com>
2024-06-23 22:57:23 +0800

    init

---
+++ MauiApp1/MauiProgram.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using MauiApp1.GraphQL;
+
+namespace MauiApp1;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+        builder.Services
+            .AddConferenceClient()
+            .ConfigureHttpClient(client => client.BaseAddress = new Uri("https://workshop.chillicream.com/graphql"));
+
+		return builder.Build();
+	}
+}
